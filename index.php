680772838a54261a21f079f1d13635ef83fe7c13
Sumeet Sharma <sksksharma0@gmail.com>
2019-07-31 11:45:24 +0530

    Adding sidebar to blog page

--- index.php
+++ index.php
@@ -5,48 +5,56 @@
 <!-- Intro -->
 
 <section class="view">
-<div class="header-image" style="width:100%">
-<img src="<?php if(!is_front_page()){
-        header_image();
-    }?>" style="width:100%" />
-</div>
-<h2 class="text-center" style="text-transform: uppercase;margin-top:2em"><b>Blogs</b></h2>
-<hr class="" style="border-width: 2px; width: 50px; border-style: groove; border-color: #ccc; border-radius: 50px; margin-bottom:2em">
-<div class="row p-3">
-        <?php
+    <div class="header-image" style="width:100%">
+    <img src="<?php if(!is_front_page()){
+            header_image();
+        }?>" style="width:100%" />
+    </div>
+    <h2 class="text-center" style="text-transform: uppercase;margin-top:2em"><b>Blogs</b></h2>
+    <hr class="" style="border-width: 2px; width: 50px; border-style: groove; border-color: #ccc; border-radius: 50px; margin-bottom:2em">
+    <div class="row p-3">
+        <div class="col-md-9 row">
+            <?php
 
-            while(have_posts()){
-                the_post();
+                while(have_posts()){
+                    the_post();
+                ?>
+                <div class="col-md-6 mb-md-0 mb-4 text-center">
+                    <!-- Featured Image -->
+                    <div class="view overlay rounded z-depth-2 mb-4">
+                        <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title(); ?>">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="mask rgba-white-slight"></div>
+                        </a>
+                    </div>  
+
+                    <!-- Post title -->
+                    <a href='<?php the_permalink(); ?>'><h4 class="font-weight-bold black-text mb-3"><strong><?php the_title(); ?></strong></h4></a>
+
+                    <!-- Post data -->
+                    <p>by <a class="font-weight-bold"><?php the_author(); ?></a></p>     
+                    <p class="dark-grey-text"><?php echo wp_trim_words(get_the_excerpt(), 50); ?></p>
+
+                    <!-- Read more button -->
+                    <a class="btn btn-primary btn-md" style="border-radius: 50px" href="<?php the_permalink(); ?>">Read more</a>           
+                </div>
+            <?php 
+                }
+                wp_reset_query();
             ?>
-            <div class="col-md-6 mb-md-0 mb-4 text-center">
-                <!-- Featured Image -->
-                <div class="view overlay rounded z-depth-2 mb-4">
-                    <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title(); ?>">
-                    <a href="<?php the_permalink(); ?>">
-                        <div class="mask rgba-white-slight"></div>
-                    </a>
-                </div>  
+        </div>
+        <div class="col-md-3 mb-4" id="sidebar">
+            <!--Sidebar-->
+            <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+                <?php dynamic_sidebar( 'sidebar' ); ?>
+            <?php endif; ?>
+            <!--/.Sidebar-->
+        </div>
+    </div>
+        
+    </div>
 
-                <!-- Post title -->
-                <a href='<?php the_permalink(); ?>'><h4 class="font-weight-bold black-text mb-3"><strong><?php the_title(); ?></strong></h4></a>
-
-                <!-- Post data -->
-                <p>by <a class="font-weight-bold"><?php the_author(); ?></a></p>     
-                <p class="dark-grey-text"><?php echo wp_trim_words(get_the_excerpt(), 50); ?></p>
-
-                <!-- Read more button -->
-                <a class="btn btn-primary btn-md" style="border-radius: 50px" href="<?php the_permalink(); ?>">Read more</a>           
-            </div>
-        <?php 
-            }
-            wp_reset_query();
-        ?>
-    
-</div>
-    
-</div>
-
-<?php mdb_pagination(); ?>
+    <?php mdb_pagination(); ?>
 
 </section>
 <!-- Intro -->
--- single.php
+++ single.php
@@ -2,87 +2,73 @@
     get_header(); 
 ?>
 
-
-
-<style type="text/css">
-    
-  
-
-</style>
-
 <?php
     while(have_posts()){
         the_post();
     ?>
     <section class="view">
 
-    <div class="row">
-
-    <div class="col-md-5">
-
-        <div class="d-flex flex-column justify-content-center align-items-center h-100">
-        <h4 class="heading display-4 p-3 text-center"><?php the_title() ?></h4>
-        <h5 class="subheading font-weight-bold text-left">Posted by: <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a> on <?php echo get_the_date(); ?></h5>
+        <!-- Post title and feature image -->
+        <div class="row">
+            <div class="col-md-5">
+                <div class="d-flex flex-column justify-content-center align-items-center h-100">
+                    <h4 class="heading display-4 p-3 text-center"><?php the_title() ?></h4>
+                    <h5 class="subheading font-weight-bold text-left">Posted by: <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a> on <?php echo get_the_date(); ?></h5>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="view zoom">
+                    <?php the_post_thumbnail( 'large', array( 'class'=> 'img-fluid z-depth-0-half mb-4')); ?> 
+                    <div class="mask flex-center hm-gradient"></div>
+                </div>
+            </div>
         </div>
 
-    </div>
-
-    <div class="col-md-7">
-
-        <div class="view zoom">
-            <?php the_post_thumbnail( 'large', array( 'class'=> 'img-fluid z-depth-0-half mb-4')); ?> 
-        <div class="mask flex-center hm-gradient">
-        </div>
-        </div>
-
-    </div>
-
-    </div>
-
-    <div class="row">
-        <div class="col-md-8 mb-4" id="content">
-            <!-- Breadcrumbs -->
-                <?php
-                    $categories = get_the_category();
-                    $first_category_name = $categories[0]->cat_name;
-                    $first_category_id = get_cat_ID( $category[0]->cat_name );
-                    $first_category_link = get_category_link( $category_id );
-                ?>
-                <ol class="breadcrumb white z-depth-1">
-                    <li class="breadcrumb-item">
-                        <a href="<?php echo get_home_url(); ?>">Home Page</a>
-                    </li>
+        <!-- Blog post content and sidebar -->
+        <div class="row container-fluid">
+            <div class="col-md-9 mb-4" id="content">
+                <!-- Breadcrumbs -->
                     <?php
-                    if (count($categories)){
-                        ?>
+                        $categories = get_the_category();
+                        $first_category_name = $categories[0]->cat_name;
+                        $first_category_id = get_cat_ID( $category[0]->cat_name );
+                        $first_category_link = get_category_link( $category_id );
+                    ?>
+                    <ol class="breadcrumb white z-depth-1">
                         <li class="breadcrumb-item">
-                            <a href="<?php echo $first_category_link ?>"><?php echo $first_category_name ?></a>
+                            <a href="<?php echo get_home_url(); ?>">Home Page</a>
                         </li>
                         <?php
-                    }
-                    ?>
-                    <li class="breadcrumb-item active"><?php the_title() ?></li>
-                </ol>
-            <!-- Breadcrumbs -->
-            <div class="post-content text-justify p-3 lead">                
-                <?php the_content(); ?>                
-            </div>
-            
-            <!--Comments and reply-->
-            <div class="comments-section">
-                <?php comments_template(); ?>
-            </div>
+                        if (count($categories)){
+                            ?>
+                            <li class="breadcrumb-item">
+                                <a href="<?php echo $first_category_link ?>"><?php echo $first_category_name ?></a>
+                            </li>
+                            <?php
+                        }
+                        ?>
+                        <li class="breadcrumb-item active"><?php the_title() ?></li>
+                    </ol>
+                <!-- Breadcrumbs -->
+                <div class="post-content text-justify p-3 lead">                
+                    <?php the_content(); ?>                
+                </div>
+                
+                <!--Comments and reply-->
+                <div class="comments-section">
+                    <?php comments_template(); ?>
+                </div>
 
-            <!--/.Comments and reply--> 
-        </div> 
-        <div class="col-md-4 mb-4" id="sidebar">
-            <!--Sidebar-->
-            <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-                <?php dynamic_sidebar( 'sidebar' ); ?>
-            <?php endif; ?>
-            <!--/.Sidebar-->
+                <!--/.Comments and reply--> 
+            </div> 
+            <div class="col-md-3 mb-4" id="sidebar">
+                <!--Sidebar-->
+                <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+                    <?php dynamic_sidebar( 'sidebar' ); ?>
+                <?php endif; ?>
+                <!--/.Sidebar-->
+            </div>
         </div>
-    </div>
     </section>
     <?php   
     }
