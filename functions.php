f254674837df23bcf799638afd0c21561d7ea8a0
Sumeet Sharma <sksksharma0@gmail.com>
2019-08-03 13:56:48 +0530

    Unhooking sale

--- functions.php
+++ functions.php
@@ -228,5 +228,8 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_
 
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 
+//content-product.php
+remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
+
 ?>
 
