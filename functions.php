f8c042ee0ea1357aea79e8ebc50c4e3fea7d9f96
Sumeet Sharma <sksksharma0@gmail.com>
2019-08-06 10:37:04 +0530

    Making links active on category page

--- functions.php
+++ functions.php
@@ -220,8 +220,6 @@ function woocommerce_template_product_short_description() {
 add_action('woocommerce_single_product_summary', 'woocommerce_template_product_short_description', 10);
 
 
-remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
-remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
 
 ?>
